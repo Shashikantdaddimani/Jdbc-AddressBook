330ca8b4d9fbab21c5e730dbac8ccc11738ce9cf
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2023-01-15 20:56:01 +0530

    Added:Logic for updating the information  about contact person

--- src/com/java/addressbook/AddressBook.java
+++ src/com/java/addressbook/AddressBook.java
@@ -55,49 +55,19 @@ public class AddressBook {
 		return addressBookList;
 
 	}
-	
-	
-	
-	public void updateCityByZip(String address, String city, String state, int zip, int id) {
-        try (Connection connection = getConnection()) {
-            Statement statement = connection.createStatement();
-            String query = "Update Address_book_database set address=" + "'" + address + "'" + ", " + "city=" + "'" + city + "'" + ", " + "state=" + "'" + state + "'" + ", " + "zip=" + zip + " where id=" + id + ";";
-            int result = statement.executeUpdate(query);
-            System.out.println(result);
-            if (result > 0) {
-                System.out.println("Address Updated Successfully");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-	   public List<ContactPerson> findAllForParticularDate(LocalDate date) {
-	        ResultSet resultSet = null;
-	        List<ContactPerson> addressBookList = new ArrayList<ContactPerson>();
-	        try (Connection connection = getConnection()) {
-	            Statement statement = connection.createStatement();
-	            String sql = "select * from Address_book_database where dateadded between cast(' "+ date + "'" +" as date)  and date(now());";
-	            resultSet = statement.executeQuery(sql);
-	            int count = 0;
-	            while (resultSet.next()) {
-	            	ContactPerson contactInfo = new ContactPerson();
-	                contactInfo.setFirstName(resultSet.getString("firstName"));
-	                contactInfo.setLastName(resultSet.getString("lastname"));
-	                contactInfo.setAddress(resultSet.getString("address"));
-	                contactInfo.setCity(resultSet.getString("city"));
-	                contactInfo.setState(resultSet.getString("state"));
-	                contactInfo.setZip(resultSet.getInt("zip"));
-	                contactInfo.setPhoneNumber(resultSet.getString("PhoneNumber"));
-	                contactInfo.setEmailId(resultSet.getString("email"));
-	                contactInfo.setBookName(resultSet.getString("bookname"));
-	                contactInfo.setDateAdded(resultSet.getDate("dateadded").toLocalDate());
 
-	                addressBookList.add(contactInfo);
-	            }
-	        } catch (SQLException e) {
-	            System.out.println(e);
-	        }
-	        return addressBookList;
-	    }
-	  
+	public void updateCityByZip(String address, String city, String state, int zip, int id) {
+		try (Connection connection = getConnection()) {
+			Statement statement = connection.createStatement();
+			String query = "Update Address_book_database set address=" + "'" + address + "'" + ", " + "city=" + "'"
+					+ city + "'" + ", " + "state=" + "'" + state + "'" + ", " + "zip=" + zip + " where id=" + id + ";";
+			int result = statement.executeUpdate(query);
+			System.out.println(result);
+			if (result > 0) {
+				System.out.println("Address Updated Successfully");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
